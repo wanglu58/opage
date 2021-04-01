87e31c0fc3e437bee4a0c3e64a1b941a9294dd8c
wanglu <wanglu@enginetech.cn>
2021-04-01 21:13:41 +0800

    “更新域名”

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
     <div class="weather_console">
       <div class="city_edit" mdui-dialog="{target: '#cities'}" v-on:click="toggleWeather">
-  			<span class="now_city_edit"></span>
+        <span class="now_city_edit"></span>
         <i class="mdui-icon material-icons" style="font-size: 16px">edit</i>
       </div>
     </div>
@@ -54,30 +54,30 @@
     </div>
   </div>
 
-	<!-- Header -->
-	<div class="header mdui-color-blue mdui-col-xs-12 mdui-toolbar">
-		<div>Timmy</div>
-		<div class="mdui-toolbar-spacer"></div>
-		<div class="weather_entry" v-on:click="toggleWeather">
-			<p class="now_city"></p>
-			<span class="now_temp"></span>
-		</div>
+  <!-- Header -->
+  <div class="header mdui-color-blue mdui-col-xs-12 mdui-toolbar">
+    <div>Timmy</div>
+    <div class="mdui-toolbar-spacer"></div>
+    <div class="weather_entry" v-on:click="toggleWeather">
+      <p class="now_city"></p>
+      <span class="now_temp"></span>
+    </div>
     <i class="mdui-icon material-icons weather_more_icon" v-bind:class="{iconHidden: hasWeather}">keyboard_arrow_down</i>
     <i class="mdui-icon material-icons weather_more_icon" v-bind:class="{iconHidden: !hasWeather}">keyboard_arrow_up</i>
-	</div>
+  </div>
 
-	<!-- Search bar -->
-	<div
+  <!-- Search bar -->
+  <div
     class="searchbar mdui-toolbar mdui-color-blue mdui-col-xs-12"
     v-bind:class="{searchbar_active: isSearch}"
   >
-		<!-- <input
-			v-on:click="toSearch"
-			v-bind:class="{searchbar_input_active: isSearch}"
-			class="mdui-textfield-input searchbar_input"
-			type="text"
-			placeholder="搜索"
-		/> -->
+    <!-- <input
+      v-on:click="toSearch"
+      v-bind:class="{searchbar_input_active: isSearch}"
+      class="mdui-textfield-input searchbar_input"
+      type="text"
+      placeholder="搜索"
+    /> -->
 
     <!-- Search engine icon & list -->
     <div
@@ -159,76 +159,76 @@
     <div class="search-button">
       <img src="./assets/img/search/search.svg" width="36px" height="36px"/>
     </div>
-	</div>
+  </div>
 
   <!-- Overlay -->
   <div class="overlay" v-on:click="closeSearch" v-bind:class="{overlay_active: isSearch}"></div>
 
   <div class="card-wrap">
-  	<!-- MDUI card -->
-  	<div class="mdui-card mdui-col-xs-12 mdui-col-md-10 mdui-col-offset-md-1 mdui-col-lg-6 mdui-col-offset-lg-1 mdui-shadow-0 sites-card ">
-  		<!-- Card title -->
-  		<div class="mdui-card-header card-header">
-  			网站
-  			<div v-on:click="editSites" class="edit">{{editOrComplete}}</div>
-  		</div>
+    <!-- MDUI card -->
+    <div class="mdui-card mdui-col-xs-12 mdui-col-md-10 mdui-col-offset-md-1 mdui-col-lg-6 mdui-col-offset-lg-1 mdui-shadow-0 sites-card ">
+      <!-- Card title -->
+      <div class="mdui-card-header card-header">
+        网站
+        <div v-on:click="editSites" class="edit">{{editOrComplete}}</div>
+      </div>
 
-  		<div class="mdui-divider"></div>
+      <div class="mdui-divider"></div>
 
-  		<!-- Sites list -->
-  		<ul class="mdui-list">
-  			<li v-for="(site, index) in sites" class="site">
-  				<button
-  					class="remove"
-  					v-bind:class="{remove_active: canRemove}"
-  					v-on:click="removeSite(index)"
-  				>
-  					<i class="mdui-icon material-icons">close</i>
-  				</button>
-  				<a v-bind:href="site.url" target="_blank">
-  					<div>
-  						<img v-bind:src="site.icon" width="55px" height="55px" v-bind:class="{remove_active: canRemove}"/>
-  					</div>
-  					<div class="site-title">{{ site.title}}</div>
-  				</a>
-  			</li>
-  			<li class="site" v-on:click="showAddpanel">
-  				<i class="mdui-icon material-icons add-site-icon" v-bind:class="{iconHidden: hasAddPanel}">add</i>
-  				<i class="mdui-icon material-icons add-site-icon" v-bind:class="{iconHidden: !hasAddPanel}">close</i>
-  				<div class="site-title">添加</div>
-  			</li>
-  		</ul>
+      <!-- Sites list -->
+      <ul class="mdui-list">
+        <li v-for="(site, index) in sites" class="site">
+          <button
+            class="remove"
+            v-bind:class="{remove_active: canRemove}"
+            v-on:click="removeSite(index)"
+          >
+            <i class="mdui-icon material-icons">close</i>
+          </button>
+          <a v-bind:href="site.url" target="_blank">
+            <div>
+              <img v-bind:src="site.icon" width="55px" height="55px" v-bind:class="{remove_active: canRemove}"/>
+            </div>
+            <div class="site-title">{{ site.title}}</div>
+          </a>
+        </li>
+        <li class="site" v-on:click="showAddpanel">
+          <i class="mdui-icon material-icons add-site-icon" v-bind:class="{iconHidden: hasAddPanel}">add</i>
+          <i class="mdui-icon material-icons add-site-icon" v-bind:class="{iconHidden: !hasAddPanel}">close</i>
+          <div class="site-title">添加</div>
+        </li>
+      </ul>
 
-  		<!-- New site panel -->
-  		<div
-  			class="new_site mdui-color-blue-grey-50"
-  			v-bind:class="{new_site_active: hasAddPanel}"
-  		>
-  			<div class="new-site-info mdui-textfield mdui-textfield-floating-label">
-  				<label class="mdui-textfield-label">网站地址</label>
-  				<input class="mdui-textfield-input" type="url" v-model="newSiteurl" />
-  			</div>
-  			<div class="new-site-info mdui-textfield mdui-textfield-floating-label">
-  				<label class="mdui-textfield-label">网站名称</label>
-  				<input class="mdui-textfield-input" type="text" v-model="newSitetitle" />
-  			</div>
-  			<div class="new-site-info mdui-textfield mdui-textfield-floating-label">
-  				<label class="mdui-textfield-label">网站图标地址</label>
-  				<input class="mdui-textfield-input" type="text" v-model="newSiteicon" />
-  			</div>
-  			<button class="addtodo_button mdui-btn mdui-btn-raised mdui-color-theme-accent mdui-ripple" v-on:click="addSite">添加</button>
-  		</div>
+      <!-- New site panel -->
+      <div
+        class="new_site mdui-color-blue-grey-50"
+        v-bind:class="{new_site_active: hasAddPanel}"
+      >
+        <div class="new-site-info mdui-textfield mdui-textfield-floating-label">
+          <label class="mdui-textfield-label">网站地址</label>
+          <input class="mdui-textfield-input" type="url" v-model="newSiteurl" />
+        </div>
+        <div class="new-site-info mdui-textfield mdui-textfield-floating-label">
+          <label class="mdui-textfield-label">网站名称</label>
+          <input class="mdui-textfield-input" type="text" v-model="newSitetitle" />
+        </div>
+        <div class="new-site-info mdui-textfield mdui-textfield-floating-label">
+          <label class="mdui-textfield-label">网站图标地址</label>
+          <input class="mdui-textfield-input" type="text" v-model="newSiteicon" />
+        </div>
+        <button class="addtodo_button mdui-btn mdui-btn-raised mdui-color-theme-accent mdui-ripple" v-on:click="addSite">添加</button>
+      </div>
 
-  	</div>
+    </div>
 
     <!-- MDUI card -->
-  	<div class="mdui-card mdui-col-xs-12 mdui-col-md-5 mdui-col-offset-md-1 mdui-col-lg-3 mdui-shadow-0 todos-card">
+    <div class="mdui-card mdui-col-xs-12 mdui-col-md-5 mdui-col-offset-md-1 mdui-col-lg-3 mdui-shadow-0 todos-card">
       <!-- Card title -->
-  		<div class="mdui-card-header card-header">
-  			待办事项
-  		</div>
+      <div class="mdui-card-header card-header">
+        待办事项
+      </div>
 
-  		<div class="mdui-divider"></div>
+      <div class="mdui-divider"></div>
 
       <ul class="mdui-list">
 
@@ -349,109 +349,109 @@ if (!Store.getCookie('visited')) {
   if(Store.fetch_site().length == 0){
     Store.save_site([
     //   {
-  		// 	"url": "https://github.com/",
-  		// 	"title": "Github",
-  		// 	"icon": require('./assets/img/icons/github.svg')
-  		// }, {
-  		// 	"url": "https://store.steampowered.com",
-  		// 	"title": "Steam",
-  		// 	"icon": require('./assets/img/icons/steam.svg')
-  		// }, {
-  		// 	"url": "https://www.wikipedia.org/",
-  		// 	"title": "Wikipedia",
-  		// 	"icon": require('./assets/img/icons/wikipedia.svg')
-  		// }, {
-  		// 	"url": "https://www.reddit.com/",
-  		// 	"title": "Reddit",
-  		// 	"icon": require('./assets/img/icons/reddit.svg')
-  		// }, {
-  		// 	"url": "https://www.flickr.com/",
-  		// 	"title": "Flickr",
-  		// 	"icon": require('./assets/img/icons/flickr.svg')
-  		// }, {
-  		// 	"url": "https://500px.com/",
-  		// 	"title": "500px",
-  		// 	"icon": require('./assets/img/icons/500.svg')
-  		// }, {
-  		// 	"url": "http://digg.com/",
-  		// 	"title": "Digg",
-  		// 	"icon": require('./assets/img/icons/digg.svg')
-  		// }, {
-  		// 	"url": "https://tumblr.com/",
-  		// 	"title": "Tumblr",
-  		// 	"icon": require('./assets/img/icons/tumblr.svg')
-  		// }, {
-  		// 	"url": "https://facebook.com/",
-  		// 	"title": "Facebook",
-  		// 	"icon": require('./assets/img/icons/facebook.svg')
-  		// }, {
-  		// 	"url": "https://twitter.com/",
-  		// 	"title": "Twitter",
-  		// 	"icon": require('./assets/img/icons/twitter.svg')
-  		// }, {
-  		// 	"url": "https://zhihu.com/",
-  		// 	"title": "知乎",
-  		// 	"icon": require('./assets/img/icons/zhihu.svg')
-  		// }, {
-  		// 	"url": "https://www.dropbox.com/",
-  		// 	"title": "Dropbox",
-  		// 	"icon": require('./assets/img/icons/dropbox.svg')
-  		// }, {
-  		// 	"url": "https://www.behance.net/",
-  		// 	"title": "Behance",
-  		// 	"icon": require('./assets/img/icons/behance.svg')
-  		// }, {
-  		// 	"url": "https://plus.google.com/",
-  		// 	"title": "Google+",
-  		// 	"icon": require('./assets/img/icons/google_plus.svg')
-  		// }, {
-  		// 	"url": "https://quora.com/",
-  		// 	"title": "Quora",
-  		// 	"icon": require('./assets/img/icons/quora.svg')
-  		// }, {
-  		// 	"url": "https://www.pinterest.com/",
-  		// 	"title": "Pinterest",
-  		// 	"icon": require('./assets/img/icons/pinterest.svg')
-  		// }, {
-  		// 	"url": "https://getpocket.com/",
-  		// 	"title": "Pocket",
-  		// 	"icon": require('./assets/img/icons/pocket.svg')
-  		// }, {
-  		// 	"url": "https://dribbble.com/",
-  		// 	"title": "Dribbble",
-  		// 	"icon": require('./assets/img/icons/dribbble.svg')
-  		// }, {
-  		// 	"url": "http://stackoverflow.com/",
-  		// 	"title": "Stack Overflow",
-  		// 	"icon": require('./assets/img/icons/stackoverflow.svg')
-  		// }, {
-  		// 	"url": "https://www.amazon.com/",
-  		// 	"title": "Amazon",
-  		// 	"icon": require('./assets/img/icons/amazon.svg')
-  		// }, {
-  		// 	"url": "https://taobao.com/",
-  		// 	"title": "淘宝",
-  		// 	"icon": require('./assets/img/icons/taobao.svg')
-  		// }, {
-  		// 	"url": "http://weibo.com/",
-  		// 	"title": "微博",
-  		// 	"icon": require('./assets/img/icons/weibo.svg')
-  		// }, {
-  		// 	"url": "http://coolapk.com/",
-  		// 	"title": "酷安",
-  		// 	"icon": require('./assets/img/icons/coolapk.svg')
-  		// }, {
-  		// 	"url": "http://douban.com/",
-  		// 	"title": "豆瓣",
-  		// 	"icon": require('./assets/img/icons/douban.svg')
-  		// }, 
+      //  "url": "https://github.com/",
+      //  "title": "Github",
+      //  "icon": require('./assets/img/icons/github.svg')
+      // }, {
+      //  "url": "https://store.steampowered.com",
+      //  "title": "Steam",
+      //  "icon": require('./assets/img/icons/steam.svg')
+      // }, {
+      //  "url": "https://www.wikipedia.org/",
+      //  "title": "Wikipedia",
+      //  "icon": require('./assets/img/icons/wikipedia.svg')
+      // }, {
+      //  "url": "https://www.reddit.com/",
+      //  "title": "Reddit",
+      //  "icon": require('./assets/img/icons/reddit.svg')
+      // }, {
+      //  "url": "https://www.flickr.com/",
+      //  "title": "Flickr",
+      //  "icon": require('./assets/img/icons/flickr.svg')
+      // }, {
+      //  "url": "https://500px.com/",
+      //  "title": "500px",
+      //  "icon": require('./assets/img/icons/500.svg')
+      // }, {
+      //  "url": "http://digg.com/",
+      //  "title": "Digg",
+      //  "icon": require('./assets/img/icons/digg.svg')
+      // }, {
+      //  "url": "https://tumblr.com/",
+      //  "title": "Tumblr",
+      //  "icon": require('./assets/img/icons/tumblr.svg')
+      // }, {
+      //  "url": "https://facebook.com/",
+      //  "title": "Facebook",
+      //  "icon": require('./assets/img/icons/facebook.svg')
+      // }, {
+      //  "url": "https://twitter.com/",
+      //  "title": "Twitter",
+      //  "icon": require('./assets/img/icons/twitter.svg')
+      // }, {
+      //  "url": "https://zhihu.com/",
+      //  "title": "知乎",
+      //  "icon": require('./assets/img/icons/zhihu.svg')
+      // }, {
+      //  "url": "https://www.dropbox.com/",
+      //  "title": "Dropbox",
+      //  "icon": require('./assets/img/icons/dropbox.svg')
+      // }, {
+      //  "url": "https://www.behance.net/",
+      //  "title": "Behance",
+      //  "icon": require('./assets/img/icons/behance.svg')
+      // }, {
+      //  "url": "https://plus.google.com/",
+      //  "title": "Google+",
+      //  "icon": require('./assets/img/icons/google_plus.svg')
+      // }, {
+      //  "url": "https://quora.com/",
+      //  "title": "Quora",
+      //  "icon": require('./assets/img/icons/quora.svg')
+      // }, {
+      //  "url": "https://www.pinterest.com/",
+      //  "title": "Pinterest",
+      //  "icon": require('./assets/img/icons/pinterest.svg')
+      // }, {
+      //  "url": "https://getpocket.com/",
+      //  "title": "Pocket",
+      //  "icon": require('./assets/img/icons/pocket.svg')
+      // }, {
+      //  "url": "https://dribbble.com/",
+      //  "title": "Dribbble",
+      //  "icon": require('./assets/img/icons/dribbble.svg')
+      // }, {
+      //  "url": "http://stackoverflow.com/",
+      //  "title": "Stack Overflow",
+      //  "icon": require('./assets/img/icons/stackoverflow.svg')
+      // }, {
+      //  "url": "https://www.amazon.com/",
+      //  "title": "Amazon",
+      //  "icon": require('./assets/img/icons/amazon.svg')
+      // }, {
+      //  "url": "https://taobao.com/",
+      //  "title": "淘宝",
+      //  "icon": require('./assets/img/icons/taobao.svg')
+      // }, {
+      //  "url": "http://weibo.com/",
+      //  "title": "微博",
+      //  "icon": require('./assets/img/icons/weibo.svg')
+      // }, {
+      //  "url": "http://coolapk.com/",
+      //  "title": "酷安",
+      //  "icon": require('./assets/img/icons/coolapk.svg')
+      // }, {
+      //  "url": "http://douban.com/",
+      //  "title": "豆瓣",
+      //  "icon": require('./assets/img/icons/douban.svg')
+      // }, 
         {
             "url": "https://www.xkblogs.com/",
             "title": "星空博客",
             "icon": require('./assets/img/icons/xkblogs.jpg')
         },
         {
-            "url": "https://www.xkssr.net/",
+            "url": "https://www.xkv2ray.com/",
             "title": "星空机场",
             "icon": require('./assets/img/icons/airlogo.png')
         },
@@ -471,9 +471,14 @@ if (!Store.getCookie('visited')) {
             "icon": require('./assets/img/icons/router.png')
         },
         {
-            "url": "https://wanglu.cloud:5001",
+            "url": "https://wanglu.cloud:888",
             "title": "群辉Nas",
             "icon": require('./assets/img/icons/nas.png')
+        },
+        {
+            "url": "http://wanglu.cloud:666",
+            "title": "个人云盘",
+            "icon": require('./assets/img/icons/zdir.png')
         },
         {
             "url": "https://www.netflix.com/",
@@ -529,15 +534,15 @@ if (!Store.getCookie('visited')) {
             "icon": require('./assets/img/icons/zhihu.svg')
         }
         
-  	])
+    ])
   }
   if(Store.fetch_todo().length == 0){
     Store.save_todo([
-  		{
-  			"label": "1. 添加你将要做的事情",
-  			"isFinished": false
-  		}
-  	])
+      {
+        "label": "1. 添加你将要做的事情",
+        "isFinished": false
+      }
+    ])
   }
 
   Store.setCookie("se", "baidu", 365);
@@ -549,58 +554,58 @@ if (!Store.getCookie('visited')) {
 
   //  console.log(Store.fetch_site().length)
 export default {
-	data() {
-		return {
+  data() {
+    return {
       editOrComplete: '编辑',
       searchEngine: Store.getCookie('se'),
-			newSiteurl: '',
-			newSitetitle: '',
-			newSiteicon: '',
+      newSiteurl: '',
+      newSitetitle: '',
+      newSiteicon: '',
       newTodo: '',
-			canRemove: false,
-			hasAddPanel: false,
-			isSearch: false,
+      canRemove: false,
+      hasAddPanel: false,
+      isSearch: false,
       hasWeather: false,
-			sites: Store.fetch_site(),
+      sites: Store.fetch_site(),
       todos: Store.fetch_todo()
-		}
-	},
-	watch: {
-		sites: {
-			handler: function(sites) {
-				Store.save_site(sites)
-			},
-			deep: true
-		},
+    }
+  },
+  watch: {
+    sites: {
+      handler: function(sites) {
+        Store.save_site(sites)
+      },
+      deep: true
+    },
     todos: {
       handler: function (todos) {
         Store.save_todo(todos)
       },
       deep: true
     }
-	},
-	methods: {
-		showAddpanel: function(){
-			this.hasAddPanel = !this.hasAddPanel;
-		},
-		addSite: function() {
-			this.sites.push({
-				url: this.newSiteurl,
-				title: this.newSitetitle,
-				icon: (this.newSiteicon ? this.newSiteicon : require('./assets/img/icons/default.svg'))
-			})
-			this.newSiteurl = ''
-			this.newSitetitle = ''
-			this.newSiteicon = ''
-		},
-		removeSite: function(index) {
-			this.sites.splice(index, 1);
-		},
-		editSites: function() {
-			this.canRemove = !this.canRemove;
+  },
+  methods: {
+    showAddpanel: function(){
+      this.hasAddPanel = !this.hasAddPanel;
+    },
+    addSite: function() {
+      this.sites.push({
+        url: this.newSiteurl,
+        title: this.newSitetitle,
+        icon: (this.newSiteicon ? this.newSiteicon : require('./assets/img/icons/default.svg'))
+      })
+      this.newSiteurl = ''
+      this.newSitetitle = ''
+      this.newSiteicon = ''
+    },
+    removeSite: function(index) {
+      this.sites.splice(index, 1);
+    },
+    editSites: function() {
+      this.canRemove = !this.canRemove;
       if(this.canRemove == true) this.editOrComplete = '完成';
       if(this.canRemove == false) this.editOrComplete = '编辑';
-		},
+    },
     addTodo: function(){
       this.todos.push({
         label: this.newTodo,
@@ -625,14 +630,14 @@ export default {
     toggleFinished: function (todo, index) {
       todo.isFinished = !todo.isFinished
     },
-		toSearch: function() {
+    toSearch: function() {
       if(this.isSearch == false){
-  			this.isSearch = true
+        this.isSearch = true
       }
-		},
+    },
     closeSearch: function() {
       if(this.isSearch == true){
-  			this.isSearch = false
+        this.isSearch = false
       }
     },
     toggleWeather: function() {
@@ -650,17 +655,17 @@ export default {
       this.searchEngine = 'baidu';
       Store.setCookie("se", "baidu", 30);
     }
-	}
+  }
 }
 
 </script>
 
 <style>
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	color: #2c3e50;
-	margin-top: 60px;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
