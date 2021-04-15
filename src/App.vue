2735a9ec3bb3226cdae99928b4359f7d579fd470
wanglu <wanglu@enginetech.cn>
2021-04-15 10:23:44 +0800

    修改导航站。

--- src/App.vue
+++ src/App.vue
@@ -523,7 +523,7 @@ if (!Store.getCookie('visited')) {
          {
             "url": "http://wanglu.cloud:8283/owdl/index.html",
             "title": "远程下载",
-            "icon": require('./assets/img/icons/thunder.png')
+            "icon": require('./assets/img/icons/wanwu.ico')
         },
          {
             "url": "https://taobao.com/",
